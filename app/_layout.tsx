cc251e34ae495945cecc12bb331b8a3e6e493a9a
Erick Pac <erick.pac@helloiconic.com>
2025-02-14 14:16:30 -0600

    add i18n support with react-i18next for localization

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,12 +1,16 @@
 import { Stack } from "expo-router";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { I18nextProvider } from "react-i18next";
+import i18n from "@/locales/i18n";
 
 const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Stack />
+      <I18nextProvider i18n={i18n}>
+        <Stack />
+      </I18nextProvider>
     </QueryClientProvider>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,9 @@
 import { Text, View } from "react-native";
+import { useTranslation } from "react-i18next";
 
 export default function Index() {
+  const { t } = useTranslation();
+
   return (
     <View
       style={{
@@ -9,7 +12,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Edit app/index.tsx to edit this screen. {t("weatherScreen")}</Text>
     </View>
   );
 }
